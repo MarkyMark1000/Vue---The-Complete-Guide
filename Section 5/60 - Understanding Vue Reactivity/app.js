388cd2f40a211eb9bb4cef8a56bd16af3764d6e0
Mark Wilson <mark.wilson@iqvia.com>
2023-09-27 15:30:48 +0100

    finish chapter5

--- Section 5/60 - Understanding Vue Reactivity/app.js
+++ Section 5/60 - Understanding Vue Reactivity/app.js
@@ -11,8 +11,19 @@ const app = Vue.createApp({
     },
     setText() {
       this.message = this.currentUserInput;
+      // THIS IS HOW WE ACCESS REF'S IN JAVASCRIPT/VUE.
+      console.log(this.$refs.userText);
     },
   },
+  /*
+  There are a set of inbuilt events that we can add vue code for the Vue App lifecyle
+  see video 66 and 67.   These include:
+  beforeCreate(), created(), beforeMount(), mounted(), beforeUpdate(), updated(),
+  beforeUnmount(), unmounted()
+  */
+ beforeCreate(){
+  console.log('beforeCreate()');
+ }
 });
 
 app.mount('#app');
