bc729dd8b2ad68953665114faceb81e0ef9f9b4e
MarkyMark1000 <mark_john_wilson@yahoo.co.uk>
2023-10-01 19:34:01 +0100

    assignment chapter8

---
+++ Section 8/101 Assignment/assignment-app/src/components/ActiveUser.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <ul>
+            <li>
+                <h2>Username: {{ userName }}</h2>
+                <h3>Age: {{ age }}</h3>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+        userName: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: String,
+            required: true
+        }}
+    };
+</script>
