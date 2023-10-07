f3fce1f69bea5fe36768b36258b1e22d762f2a02
MarkyMark1000 <mark_john_wilson@yahoo.co.uk>
2023-10-07 16:42:38 +0100

    Finish 127, basic setup

---
+++ Section 10/setup/new_project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <ul>
+        <li v-for="resource in storedResources" :key="resource.id">
+            <h2>
+                <a :href="resource.link">
+                    {{ resource.id + ' ' + resource.title }}
+                </a>
+            </h2>
+            <h4>{{ resource.description }}</h4>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official vue.js documentation',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn to google',
+                    link: 'https://google.org'
+                },
+            ],
+        };
+    },
+}
+</script>
