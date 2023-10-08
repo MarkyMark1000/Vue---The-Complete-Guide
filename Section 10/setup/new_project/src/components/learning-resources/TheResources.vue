297e866df2afee07f9516deb4206aec9fbcda010
MarkyMark1000 <mark_john_wilson@yahoo.co.uk>
2023-10-08 16:19:17 +0100

    132 he needs to make adjustments yet

---
+++ Section 10/setup/new_project/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,22 @@
+<template>
+    <base-card>
+        <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
+        <base-button @click="setSelectedTab('add-resource')">Add Resource</base-button>
+    </base-card>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            selectedTab: 'stored-resources'
+        };
+    },
+    methods: {
+        setSelectedTab(new_tab) {
+            this.selectedTab = new_tab;
+        }
+    }
+}
+</script>
