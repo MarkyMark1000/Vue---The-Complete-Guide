3918e4be3b5c7fd2f63d537497d0747e53e27ab0
Mark Wilson <mark.wilson@iqvia.com>
2023-10-29 14:41:08 +0000

    up to 203

--- Section 14/animations/src/App.vue
+++ Section 14/animations/src/App.vue
@@ -24,16 +24,41 @@
       Key thing - transition is expecting one element inside to attach
       the classes to.   Problem when using custom components that actually
       contain multiple elements next to each other.
+
+      GREEN SOCK - 3rd Party Javascript Animation Library
     -->
-    <transition>
+    <transition
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @before-leave="beforeLeave"
+      @after-leave="afterLeave"
+      @leave="leave"
+      @enter-cancelled="enterCancelled"
+      @leave-cancelled="leaveCancelled">
       <p v-if="paraIsVisible"> This is only sometimes visible</p>
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
-  <base-modal @close="hideDialog" :open="dialogIsVisible">
-    <p>This is a test dialog!</p>
-    <button @click="hideDialog">Close it!</button>
+  <base-modal
+    @close="hideDialog"
+    :open="dialogIsVisible">
+      <p>This is a test dialog!</p>
+      <button @click="hideDialog">Close it!</button>
   </base-modal>
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <!--
+        can use transition containing 2 elements if you can guarantee that only
+        one element will be added to the DOM at any one time!!!
+        
+        mode controls transition of elements.   out-in removes element then adds
+        new one, in-out does the reverse.
+      -->
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
@@ -46,9 +71,76 @@ export default {
       animatedBlock: false,
       dialogIsVisible: false,
       paraIsVisible: false,
+      usersAreVisible: false,
+      enterInterval: null,
+      leaveInterval: null,
     };
   },
   methods: {
+    enterCancelled() {
+      console.log('enterCancelled');
+      clearInterval(this.enterInterval);
+    },
+    leaveCancelled() {
+      console.log('leaveCancelled');
+      clearInterval(this.leaveInterval);
+    },
+    beforeEnter(el) {
+      console.log('beforeEnter()');
+      console.log(el);
+      el.style.opacity = 0;
+    },
+    afterEnter(el) {
+      console.log('afterEnter()');
+      console.log(el);
+    },
+    enter(el, done) {
+      console.log('enter()');
+      console.log(el);
+      let round = 1;
+      this.enterInterval = setInterval(
+        () => {
+          el.style.opacity = round*0.01;
+          round++;
+          if(round>100){
+            clearInterval(this.enterInterval);
+            done();
+          }
+        },
+        20
+      );
+    },
+    beforeLeave(el) {
+      console.log('beforeLeave()');
+      console.log(el);
+      el.style.opacity=1;
+    },
+    leave(el, done) {
+      console.log('leave()');
+      console.log(el);
+      let round = 100;
+      this.leaveInterval = setInterval(
+        () => {
+          el.style.opacity = round*0.01;
+          round--;
+          if(round<0){
+            clearInterval(this.leaveInterval);
+            done();
+          }
+        },
+        20
+      );
+    },
+    afterLeave(el) {
+      console.log('afterLeave()');
+      console.log(el);
+    },
+    showUsers() {
+      this.usersAreVisible=true;
+    },
+    hideUsers() {
+      this.usersAreVisible=false;
+    },
     toggleParagraph() {
       this.paraIsVisible = !this.paraIsVisible;
     },
@@ -138,6 +230,20 @@ button:active {
   transform: translateY(-30px); */
 }
 
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
+}
 /* A MORE DETAILED TRANSFORMATION */
 @keyframes slide-fade {
   0% {
